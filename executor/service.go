22eaa85472ae6ee22e2753b14e270a11f4a5007a
alex538 <alexey.lunkov@gmail.com>
2024-06-08 22:02:45 +0300

    add workers, clarify api

--- executor/service.go
+++ executor/service.go
@@ -9,7 +9,12 @@ import (
 
 type Executor interface {
 	Schedule(fn func()) error
-	Stop()
+	Stop() error
+}
+
+type ExecutorConfig struct {
+	QueueSize uint32
+	PoolSize  uint32
 }
 
 type executorServiceStatus int32
@@ -23,37 +28,61 @@ type executorService struct {
 	mu     sync.Mutex
 }
 
-func CreateExecutor() Executor {
-	var queue = make(chan func(), 100)
+// Creates "Executor" instance with a specified configuration
+func CreateExecutor(cfg ExecutorConfig) (Executor, error) {
+	if cfg.PoolSize < 1 {
+		return nil, errors.New("pool size cannot be less than 1")
+	}
+	if cfg.QueueSize < 1 {
+		return nil, errors.New("queue size cannot be less that 1")
+	}
+
+	var queue = make(chan func(), cfg.QueueSize)
 	es := &executorService{status: running, queue: queue}
 
-	go infinitChannelReader(es)
-	return es
+	go initializeWorkers(es, cfg)
+
+	return es, nil
 }
 
-func infinitChannelReader(es *executorService) {
+func worker(index int, es *executorService, wait *sync.WaitGroup) {
+	defer wait.Done()
 	for {
 		if es.status == stopped {
-			log.Println("service has been stopped, terminating a listener")
+			log.Println("gcr[", index, "] service has been stopped, terminating a listener")
 			break
 		}
 
-		nfn := <-es.queue
+		nfn, ok := <-es.queue
 
-		if nfn != nil {
+		if nfn != nil && ok {
 			go nfn()
 		} else {
-			log.Println("Received nil value, terminating a listener")
+			log.Println("gcr[", index, "] Received nil value, terminating a listener")
 		}
 	}
+}
 
-	log.Println("closing channel")
+func initializeWorkers(es *executorService, cfg ExecutorConfig) {
+	var wait sync.WaitGroup
+	for i := 0; i < int(cfg.PoolSize); i++ {
+		wait.Add(1)
+		go worker(i, es, &wait)
+	}
 
+	// wait when all workers are done
+	wait.Wait()
+
+	// then close a queue's channel
 	if es.queue != nil {
+		log.Println("closing channel")
 		close(es.queue)
 	}
 }
 
+// Add a new function, to be executed, into a queue
+// Return an error if not possible to schedule:
+// - service is stopped
 func (es *executorService) Schedule(fn func()) error {
 	if fn == nil {
 		return errors.New("function cannot be nil")
@@ -71,12 +100,19 @@ func (es *executorService) Schedule(fn func()) error {
 	return nil
 }
 
-func (es *executorService) Stop() {
+// Stops executor service
+// sends a stop signal to each worker
+func (es *executorService) Stop() error {
 	es.mu.Lock()
 	defer es.mu.Unlock()
+	log.Println("stopping an executor")
 
 	if es.status == running {
 		atomic.StoreInt32((*int32)(&es.status), int32(stopped))
 		es.queue <- nil
+	} else {
+		return errors.New("service already stopped")
 	}
+
+	return nil
 }
