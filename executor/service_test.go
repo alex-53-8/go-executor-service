15e1ed6e8ae5194d6e0be512d032bae6b031b930
alex538 <alexey.lunkov@gmail.com>
2024-06-18 20:12:07 +0300

    add extra unit tests

--- executor/service_test.go
+++ executor/service_test.go
@@ -9,12 +9,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const queueSize = 10000
+const poolSize = 4
+
 func Test_GeneralUsage(t *testing.T) {
 	var onDoneWait sync.WaitGroup
 	onDoneWait.Add(1)
-
-	queueSize := 10000
-	poolSize := 4
 
 	// creating an executor
 	cfg := ExecutorConfig{PoolSize: poolSize, QueueSize: queueSize, OnAllWorkersStopped: func() {
@@ -53,4 +53,61 @@ func Test_GeneralUsage(t *testing.T) {
 
 	// a final accord - we are here only after `onDoneWait` is passed
 	assert.True(t, true)
+}
+
+func Test_CreateAndStopEmptyService(t *testing.T) {
+	var onDoneWait sync.WaitGroup
+	onDoneWait.Add(1)
+
+	// creating an executor
+	cfg := ExecutorConfig{PoolSize: poolSize, QueueSize: queueSize, OnAllWorkersStopped: func() {
+		// called when all workers are exited
+		log.Println("on all workers are done")
+		onDoneWait.Done()
+	}}
+
+	executor, _ := CreateExecutor(cfg)
+
+	// service is not stopped yet, so no error is expected here
+	assert.Nil(t, executor.Stop())
+
+	// stopping again a service returns an error
+	assert.Equal(t, "service already stopped", executor.Stop().Error())
+
+	onDoneWait.Wait()
+
+	// ensure that nothing was processed
+	assert.Equal(t, int32(0), executor.ProcessedCount())
+}
+
+func Test_ScheduleNilValuesReturnsError(t *testing.T) {
+	var onDoneWait sync.WaitGroup
+	onDoneWait.Add(1)
+
+	// creating an executor
+	cfg := ExecutorConfig{PoolSize: poolSize, QueueSize: queueSize, OnAllWorkersStopped: func() {
+		// called when all workers are exited
+		log.Println("on all workers are done")
+		onDoneWait.Done()
+	}}
+
+	executor, _ := CreateExecutor(cfg)
+
+	// schedule not nil work
+	assert.Nil(t, executor.Schedule(func() {
+		log.Println("I'm a lambda function")
+	}))
+	// in case of nil work an error is returned
+	assert.NotNil(t, executor.Schedule(nil))
+
+	// sleep for a short time - let a goroutine to be picked up for execution
+	time.Sleep(time.Duration(50) * time.Millisecond)
+
+	// stopping
+	assert.Nil(t, executor.Stop())
+
+	onDoneWait.Wait()
+
+	// ensure one scheduled piece was processed
+	assert.Equal(t, int32(1), executor.ProcessedCount())
 }
